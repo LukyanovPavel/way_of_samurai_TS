7e62e806f24657f6564702adf001b89281b2b122
p.lukyanov <p.lukyanov@qa-academy.by>
2022-02-17 16:32:43 +0300

    add NabLink active

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
             <Navbar/>
             <div className="app-wrapper-content">
                 <Routes>
-                    <Route path={'/profile'} element={<Dialogs/>}/>
-                    <Route path={'/dialogs'} element={<Profile/>}/>
+                    <Route path={'/profile'} element={<Profile/>}/>
+                    <Route path={'/dialogs'} element={<Dialogs/>}/>
                     <Route path={'/news'} element={<News/>}/>
                     <Route path={'/music'} element={<Music/>}/>
                     <Route path={'/settings'} element={<Settings/>}/>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import s from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
+
+type isActiveType = {
+    isActive: boolean
+}
 
 export const Navbar = () => {
+
+    const setActive =({isActive}: isActiveType) => {
+       return  isActive ? s.active : s.item
+    }
+
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href={'/profile'}>Profile</a>
+                <NavLink to={'/profile'} className={setActive}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/dialogs'}>Messages</a>
+                <NavLink to={'/dialogs'} className={setActive}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/news'}>News</a>
+                <NavLink to={'/news'} className={setActive}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/music'}>Music</a>
+                <NavLink to={'/music'} className={setActive}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href={'/settings'}>Settings</a>
+                <NavLink to={'/settings'} className={setActive}>Settings</NavLink>
             </div>
         </nav>
     )
